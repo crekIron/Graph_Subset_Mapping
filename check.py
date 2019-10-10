9bade9c71d6a949ada07a4ec582ba4a4251dee55
Shreya Sharma <34606101+ShreyaSharma99@users.noreply.github.com>
2019-10-10 18:58:09 +0530

    new

--- check.py
+++ check.py
@@ -11,165 +11,69 @@ Adjust these parameters by hand.
   the argparse package will not run on attu.cs.washington.edu
 """
 
-G1nodes = int(sys.argv[1])  # number of nodes in larger graph
-G1edges = int(sys.argv[2])  # number of edges in larger graph
-G2nodes = int(sys.argv[3])  # number of nodes in smaller graph
-G2edges = int(sys.argv[4])  # number of edges in smaller graph
+gFile = str(sys.argv[1]) # test input graphs
+mapFile = str(sys.argv[2]) # output mapping
 
-guarantee_subgraph = True  # whether to deliberately make G2 a subgraph of G1
-allow_self_edges = False  # if true, a node can have an edge to itself
-print_digraph = False  # if True, print in graphviz format for visualization
-do_xform = True  # Sachin: Remove isolated nodes from Graphs
+def AddNode(G,a):
+	if(a[0] in G):
+		G[a[0]].append(a[1]);
+	else:
+		G[a[0]]=[a[1]];
+	if(not(a[1] in G)):
+		G[a[1]]=[];
 
+def readGraph(fname):
+	f=open(fname);
+	G1=dict();
+	G2=dict();
+	chk=-1;
+	for lin in f.readlines():
+		l=lin.lstrip().rstrip();		
+		l=l.split();
+		l=[int(a) for a in l];
+		if((l[0]==0) and (l[1]==0)):
+			chk=1;
+			continue;
+		if(chk<0):
+			AddNode(G1,l);
+		else:
+			AddNode(G2,l);
+	f.close();
+	return [G1,G2];
 
-def makeGraph(nodeCount, edgeCount):
-    if edgeCount > pow(nodeCount, 2):
-        print "Can't get ", edgeCount, " edges on ", nodeCount, " nodes."
-        raise Exception
-    if (not allow_self_edges) and edgeCount > math.factorial(nodeCount):
-        print "Can't get ", edgeCount, " edges on ", nodeCount, " nodes without edges to self."
-        raise Exception
-    nodeList = range(1, nodeCount+1)
-    edges = []
-    for e in range(edgeCount):
-        done = False
-        while not done:
-            N1 = random.choice(nodeList)
-            N2 = random.choice(nodeList)
-            done = (N1, N2) not in edges and (allow_self_edges or N1 != N2)
-        edges.append((N1, N2))
-    return edges
+def readMap(fname):
+	f=open(fname);
+	Map=dict();
+	for lin in f.readlines():
+		l=lin.lstrip().rstrip();		
+		l=l.split();
+		l=[int(a) for a in l];
+		Map[l[0]]=l[1];
+	f.close();
+	return Map;
 
-
-def makeSuperGraph(G1nodes, G1edges, G2nodes, G2edges, G2):
-    if G1nodes < G2nodes or G1edges < G2edges:
-        print "First Graph should have more nodes than second graph"
-        raise Exception
-    oldNodes = range(1, G2nodes+1)
-    newNodes = range(G2nodes+1, G1nodes+1)
-    allNodes = range(1, G1nodes+1)
-
-    newEdges = []
-    for i in range(G1edges - G2edges):
-        done = False
-        while not done:
-            n1 = random.choice(newNodes)
-            n2 = random.choice(allNodes)
-            if random.choice([True, False]):
-                e = (n1, n2)
-            else:
-                e = (n2, n1)
-            done = e not in newEdges and (allow_self_edges or n1 != n2)
-        newEdges.append(e)
-        superGraph = newEdges+G2
-        # superGraph.sort() # if you want to obfuscate the subgraph
-    return superGraph
-    SubGraph = copy.copy(Graph)
-    for i in range(len(Graph) - n):
-        e = random.choice(SubGraph)
-        SubGraph.remove(e)
-    return SubGraph
-
-
-def renameGraph(N, Graph):
-    NewGraph = []
-    Nodes = range(1, N+1)
-    random.shuffle(Nodes)
-    translator = {}
-    for e in Graph:
-        n1 = Nodes[e[0]-1]
-        n2 = Nodes[e[1]-1]
-        NewGraph.append((n1, n2))
-    return NewGraph
-
-
-def printSubgraphs(G1, G2):
-    print "digraph {"
-
-    print '\tsubgraph clusterG1 { graph [label="G"]'
-    for e in G1:
-        print "\t\t",  "G"+str(e[0]), "->", "G"+str(e[1])
-    print "\t}"
-
-    print '\tsubgraph clusterG2 { graph [label="g"]'
-    for e in G2:
-        print "\t\t",  "g"+str(e[0]), "->", "g"+str(e[1])
-    print "\t}"
-
-    print "}"
-
-
-def printDigraph():
-    print "digraph {"
-    for e in edges:
-        print "\t",  e[0], "->", e[1]
-    print "}"
-
-
-def printGraph(edges):
-    for e in edges:
-        print e[0], e[1]
-
-
-# Sachin: Transform graphs to remove the isolated nodes
-def AddNode(V, e):
-    if(e[0] not in V):
-        V.append(e[0])
-    if(e[1] not in V):
-        V.append(e[1])
-
-
-def ReplaceNode(G, n, n2):
-    for i in range(len(G)):
-        G[i] = (n2 if G[i][0] == n else G[i][0],
-                n2 if G[i][1] == n else G[i][1])
-
-
-def xform(G):
-    V = []
-    for e in G:
-        AddNode(V, e)
-    V.sort()
-    V2 = []
-    idx = 1
-    for n in V:
-        while idx < n:
-            V2.append(idx)
-            idx += 1
-        idx += 1
-    # print 'Connected Nodes', V
-    # print 'Isolated  Nodes', V2
-    # Now replace the last nodes to isolated nodes
-    idx = len(V)-1
-    for n2 in V2:
-        n = V[idx]
-        if n < n2:
-            break
-        idx -= 1
-        ReplaceNode(G, n, n2)
-
+def CheckMap(M,G1,G2):
+	try:
+		n	=	M.keys();
+		n1	=	[M[a] for a in n];
+		if(not(len(n1) == len(set(n1)))):
+			return False;
+		for i in n:
+			for j in n:
+				if(not (i==j)):
+					if(not((j in G2[i]))==(M[j] in G1[M[i]])):
+						return False;
+		return True;
+	except KeyError:
+		print("Bad Input: Invalid Node Id");
+		return False;
 
 def main():
 
-    G2 = makeGraph(G2nodes, G2edges)
-    if guarantee_subgraph:
-        G1 = makeSuperGraph(G1nodes, G1edges, G2nodes, G2edges, G2)
-        G2 = renameGraph(G2nodes, G2)
-    else:   # G1 and G2 are both random graphs
-        G1 = makeGraph(G1nodes, G1edges)
-
-    # Sachin: Transform graphs to remove the isolated nodes
-    if do_xform:
-        xform(G1)
-        xform(G2)
-
-    if print_digraph:
-        printSubgraphs(G1, G2)
-    else:
-        printGraph(G1)
-        print "0 0"
-        printGraph(G2)
-
+  	Res=readGraph(gFile);
+  	Map=readMap(mapFile);	
+  	print(CheckMap(Map,Res[0],Res[1]));
+	
 
 if __name__ == "__main__":
     main()
